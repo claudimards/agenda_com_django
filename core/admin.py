eab895d5f8e3963282b0ca0610d50091f685256c
claudimards <claudimards@gmail.com>
2020-06-18 22:46:21 -0300

    Criado tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+# classe para definir quais campos mostrar de titulo
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ( 'usuario', 'data_evento', )
+
+# registrando a tabela no banco de dados
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User # importando usuarios do banco de dados
 
 # Create your models here.
+
+# método para criar um modelo de tabela para o banco de dados
+class Evento(models.Model):
+    titulo = models.CharField(max_length = 100)
+    descricao = models.TextField(blank = True, null = True)
+    data_evento = models.DateTimeField(verbose_name = 'Data do Evento') #verbose modifica o nome apresentado na coluna na tabela
+    data_criacao = models.DateTimeField(auto_now = True, verbose_name = 'Data de criação')
+    # atribuindo usuario ao campo de chave estrangeira da tabela
+    usuario = models.ForeignKey( User, on_delete = models.CASCADE ) # on_delete obriga a tabela a apagar todos registroso do usuario ao ser deletado
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
