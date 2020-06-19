086c72021b6805d5e3f6f9b05c449dc4102600f8
claudimards <claudimards@gmail.com>
2020-06-19 11:52:26 -0300

    Adicionado funções de alteração e exclusão de dados

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from core.models import Evento
 
 # classe para definir quais campos mostrar de titulo
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_display = ( 'id', 'titulo', 'data_evento', 'data_criacao' )
     list_filter = ( 'usuario', 'data_evento', )
 
 # registrando a tabela no banco de dados
